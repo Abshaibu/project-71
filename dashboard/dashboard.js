0d292d771edbfa54b15c782b70f4a93a4e54c315
Shaibu Abdulsalam <shaibuabdulsalam56@gmail.com>
2022-08-06 13:19:59 +0100

    completed dashboard

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,7 +1,10 @@
 const expander = document.querySelector('.sidebar-expander');
 const sidebar = document.querySelector('.sidebar');
 const aside = document.querySelector('aside');
+const openModal = document.querySelector('.modal-wrapper');
 const toggler = document.querySelectorAll('.toggler');
+const newDocModal = document.querySelectorAll('.new-doc');
+const body = document.querySelector('body');
 
 
 // Sidebar Toggler
@@ -15,6 +18,60 @@ toggler.forEach(toggle => {
     toggle.addEventListener('click', () => {
         aside.classList.toggle('navbar-toggler');
     })
+})
+
+// New Document Modal
+newDocModal.forEach(newDoc => {
+    newDoc.addEventListener('click', () => {
+        openModal.classList.toggle('modal');
+        body.classList.toggle('no-scroll');
+    })
+})
+
+// Business Details Form
+const bizDetails = document.querySelector(".business-details");
+const companyDetails = {};
+bizDetails.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // e.stopPropagation()
+
+    companyDetails.name = document.querySelector('#bizname').value;
+    companyDetails.phone = document.querySelector('#phone').value;
+    companyDetails.email = document.querySelector('#email').value;
+    companyDetails.website = document.querySelector('#url').value;
+    console.log(companyDetails);
+    openModal.classList.add('changes')
+    bizDetails.classList.add('terms-features');
+    bizDetails.classList.remove('business-details');
+})
+
+// Terms Features
+const details = document.querySelector(".details");
+const features = {};
+details.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(details);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    openModal.classList.add('add-progress');
+    console.log(features);
+    // openModal.classList.remove('changes')
+    // openModal.classList.toggle('modal');
+    // body.classList.toggle('no-scroll');
+
+    // close and show preview page
+})
+
+// Back to Business Details
+const backBtn = document.querySelector('.back');
+backBtn.addEventListener('click', (e) => {
+    console.log(e.target); 
+    openModal.classList.remove('changes');
+    openModal.classList.remove('add-progress');
+    bizDetails.classList.remove('terms-features');
+    bizDetails.classList.add('business-details');
 })
 
 // Tabs Switcher
